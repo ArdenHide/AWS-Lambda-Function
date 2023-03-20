25a716a91b4dff62e228c19c9fbff2145396d78f
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2023-03-20 23:58:56 +0300

    add project

---
+++ AWSLambdaFunction/LambdaFunction.cs
@@ -0,0 +1,22 @@
+using Amazon.Lambda.Core;
+using Newtonsoft.Json.Linq;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace AWSLambdaFunction
+{
+    public class LambdaFunction
+    {
+        public JToken Run(object? message, ILambdaContext context)
+        {
+            var data = new JObject()
+            {
+                new JProperty("message", message)
+            };
+
+            context.Logger.Log($"{LogLevel.Debug}", $"{data}");
+
+            return data;
+        }
+    }
+}
